4f46edb698e09c8803d61345b820856740e01113
Davis O. <12512405+davisokoth@users.noreply.github.com>
2024-10-22 11:46:52 +0300

    Update rails_setup_fresh_install.sh to add nvm home
    This was recommended when I ran the command to install nvm

--- rails_setup_fresh_install.sh
+++ rails_setup_fresh_install.sh
@@ -12,6 +12,7 @@ cd maisha-meds-rails-api/
 sudo apt install curl
 source ~/.bashrc
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
 nvm install v20.18.0
 sudo apt-get install openssl
 sudo sudo apt install libpq-dev
